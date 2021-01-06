82f1984c22ae895981d9fae0a8e6e83d3038df17
Jesse Sargent <mountaneer12@gmail.com>
2021-01-06 10:18:54 -0800

    Added recursive Insertion Sort implementation

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -24,6 +24,26 @@ public class InsertionSort {
 		}
 	}
 	
+	public static void recursiveInsertionSort(int[] inputArr, int numItems) {
+		
+		if(numItems < 2) {
+			return;
+		}
+		
+		recursiveInsertionSort(inputArr, numItems - 1);
+		
+	
+			int newElement = inputArr[numItems - 1]; // value of the first unsorted index
+
+			int i;
+
+			for (i = numItems - 1; i > 0 && inputArr[i - 1] > newElement; i--) {
+				inputArr[i] = inputArr[i - 1];
+			}
+
+			inputArr[i] = newElement;
+	}
+	
 	// POPULATE ARRAY METHOD
 	public static void populateArray(int[] array) {
 		for (int i = 0; i < array.length; i++) {
@@ -40,17 +60,28 @@ public class InsertionSort {
 
 	public static void main(String[] args) {
 		
+		// test arrays
 		int[] testArray = new int[10];
+		int[] testArray2 = new int[10];
 
-		// unsorted array
+		System.out.println("ITERATIVE INSERTION SORT");
 		populateArray(testArray);
 		printArray(testArray);
-
 		System.out.println();
 
-		// sorted array
+		// iterative insertion sort
 		insertionSort(testArray);
 		printArray(testArray);
+		System.out.println("\n");
+		
+		System.out.println("RECURSIVE INSERTION SORT");
+		populateArray(testArray2);
+		printArray(testArray2);
+		System.out.println();
+		
+		// recursive insertion sort
+		recursiveInsertionSort(testArray2, 10);
+		printArray(testArray2);
 	}
 
 }
