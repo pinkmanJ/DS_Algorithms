63986f0ee01a8761e17afdb419fe6a47fdaae35c
Jesse Sargent <mountaneer12@gmail.com>
2021-01-06 10:19:41 -0800

    Added analysis comment to CountingSort file

--- src/CountingSort.java
+++ src/CountingSort.java
@@ -1,24 +1,30 @@
-
+/*
+ * COUNTING SORT ANALYSIS
+ * Makes assumptions about the data
+ * No comparisons
+ * Only works with non-negative discrete values (whole numbers)
+ * Values must be in a specific range
+ */
 public class CountingSort {
-	
+
 	public static void countingSort(int[] inputArr, int min, int max) {
-		
+
 		int[] countArr = new int[(max - min) + 1];
-		
+
 		// fill count array with respective values
-		for(int i = 0; i < inputArr.length; i++) {
+		for (int i = 0; i < inputArr.length; i++) {
 			countArr[inputArr[i] - min]++;
 		}
-		
+
 		int j = 0;
-		for(int i = min; i <= max; i++) {
-			while(countArr[i - min] > 0) {
+		for (int i = min; i <= max; i++) {
+			while (countArr[i - min] > 0) {
 				inputArr[j++] = i;
 				countArr[i - min]--;
-			}		
+			}
 		}
 	}
-	
+
 	// POPULATE ARRAY METHOD
 	public static void populateArray(int[] array) {
 		for (int i = 0; i < array.length; i++) {
@@ -34,7 +40,7 @@ public class CountingSort {
 	}
 
 	public static void main(String[] args) {
-		
+
 		int[] testArray = new int[10];
 
 		// unsorted array
@@ -48,5 +54,4 @@ public class CountingSort {
 		printArray(testArray);
 
 	}
-
 }
